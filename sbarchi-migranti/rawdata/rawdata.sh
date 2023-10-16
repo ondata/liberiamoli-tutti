814f28a2c2d15c71ce29977b5fe40a7fba540a3a
aborruso <aborruso@gmail.com>
2023-10-16 18:13:30 +0200

    update

--- sbarchi-migranti/rawdata/rawdata.sh
+++ sbarchi-migranti/rawdata/rawdata.sh
@@ -7,7 +7,10 @@ set -o pipefail
 
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-mlr --csv sort -r Data_Report,Data "$folder"/csv/*.csv > "$folder"/../sbarchi-migranti.csv
+mlr --csv sort -f Data_Report,Data "$folder"/csv/*.csv > "$folder"/../sbarchi-migranti-vit.csv
 
-mlr --c2m head -n 5 "$folder"/../sbarchi-migranti.csv > "$folder"/../tmp.md
+mlr --c2m head -n 5 "$folder"/../sbarchi-migranti-vit.csv > "$folder"/../tmp.md
 
+mlr --csv sort -f Data_Report,Data "$folder"/../script/data/*.csv > "$folder"/../script/sbarchi-migranti-dbc.csv
+
+daff --output "$folder"/../script/sbarchi-migranti.html "$folder"/../sbarchi-migranti-vit.csv "$folder"/../script/sbarchi-migranti-dbc.csv
