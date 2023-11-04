ce01ee5b6edcab11a41f2874605ddd1cc13c54f8
aborruso <aborruso@gmail.com>
2023-11-04 13:20:15 +0100

    update permessi

--- sbarchi-migranti/script/bulk_sbarchi-migranti.sh
+++ sbarchi-migranti/script/bulk_sbarchi-migranti.sh
@@ -13,36 +13,11 @@ mkdir -p "$folder"/tmp
 mkdir -p "$folder"/tmp/check
 mkdir -p "$folder"/data
 
-URL="http://www.libertaciviliimmigrazione.dlci.interno.gov.it/it/documentazione/statistica/cruscotto-statistico-giornaliero"
-
-# se URL non risponde esci
-curl -kL -s -f -o /dev/null "$URL" || exit 0
-
-# estrai la lista dei file pdf presenti nella pagina
-curl -kL "$URL" | scrape -be "//div[contains(@class, 'field-item')]//a[contains(@href, '.pdf')]" | tail -n +2 | xq -c '.html.body.a[]' >"$folder"/data/cruscotto-statistico-giornaliero_lista_raw.jsonl
-
-# aggiungi la data di pubblicazione del report, in formato YYYY-MM-DD. Se non è presente, usa la data di default 30-08-2000
-# rimuovi file pdf del giorno, mantieni soltanto i quindicinali
-mlrgo --jsonl put '$data=strftime(strptime(regextract_or_else(${@title},"[0-9]{2}-[0-9]{2}-[0-9]{4}","30-08-2000"),"%d-%m-%Y"),"%Y-%m-%d")' "$folder"/data/cruscotto-statistico-giornaliero_lista_raw.jsonl >"$folder"/data/cruscotto-statistico-giornaliero_lista.jsonl
-
-# estrai la lista dei file pdf, in cui è presente il dato dei sbarchi giornalieri. Ci sono dal report del 15/10/2019
-# rimuovi il report del 15/07/2021, perché il PDF è danneggiato
-# rimuovi la prima riga perché è quella del giorno. Teniamo soltanto i dati quindicinali
-mlrgo --jsonl filter '$data>"2019-09-16"' then filter -x '$data=="2021-07-15"' then sort -f data "$folder"/data/cruscotto-statistico-giornaliero_lista.jsonl | head -n -1 >"$folder"/data/cruscotto-statistico-giornaliero_lista_dati_giornalieri.jsonl
-
-
-# scarica i file pdf, se non sono già presenti
-while read -r line; do
-  url_file=$(echo "$line" | jq -r '."@href"')
-  wget -nc -P "$folder"/../../"$progetto"/rawdata/pdf/ "$url_file"
-done < "$folder"/data/cruscotto-statistico-giornaliero_lista_dati_giornalieri.jsonl
-
-exit 0
-
-<"$folder"/data/cruscotto-statistico-giornaliero_lista_dati_giornalieri.jsonl head -n 5 >"$folder"/data/tmp.jsonl
-
+# pulisci cartella check da html e txt
 find "$folder"/tmp/check/ -type f -name '*.html' -exec rm {} \;
 find "$folder"/tmp/check/ -type f -name '*.txt' -exec rm {} \;
+
+# pulisci cartella data da csv
 find "$folder"/data -type f -name '*.csv' -exec rm {} \;
 
 # estrai i dati dai file pdf
@@ -51,11 +26,21 @@ while read -r line; do
   data=$(echo "$line" | jq -r '.data')
   anno=$(echo "$data" | awk -F- '{print $1}')
   mese=$(echo "$data" | awk -F- '{print $2}')
+
+  # estrai numero pagina con i migranti sbarcati per giorno
   pagina=$(pdfgrep -n "per giorno" "$folder"/../../"$progetto"/rawdata/pdf/"$file" | awk -F: '{print $1}' | sort | uniq)
+
+  # converti la pagina in xml
   pdftohtml -xml -c  -f "$pagina" -l "$pagina" "$folder"/../../"$progetto"/rawdata/pdf/"$file" "$folder"/tmp.xml
+
+  # estrai i dati dall'xml
   <"$folder"/tmp.xml grep -P '<b>[0-9].*</b>' | sed -r 's/^(.+)<b>([0-9].+?)<\/b>(.+)/\2/g' | tr ' ' '\n' | sed '/^$/d'  >"$folder"/tmp_raw.txt
+
   cp "$folder"/tmp_raw.txt "$folder"/tmp/check/tmp_raw-"$data".txt
+
+  # Tieni dell'XML soltanto le serie di dati che hanno a che fare con i giorni
   sed -i -n '1N;$!N;/1\n2\n3/q;P;D' "$folder"/tmp_raw.txt
+
   # correggi i vari errori dovuti alle stranezze nei PDF, vedi "sbarchi-migranti/note"
   if [ "$data" = "2020-07-15" ]; then
     echo "0" >> "$folder"/tmp_raw.txt
@@ -83,6 +68,7 @@ while read -r line; do
     # aggiungi riga vuota alla penultima riga
     sed -i '$i\ ' "$folder"/tmp_raw.txt
   fi
+
   mlrgo --csv --implicit-csv-header cat -n then label Data,Valore then clean-whitespace then \
   put '$Valore=gsub(string($Valore),"\.","")' then \
   put '$Data=fmtnum($Data,"%02d")' then \
@@ -101,12 +87,19 @@ done < "$folder"/data/cruscotto-statistico-giornaliero_lista_dati_giornalieri.js
 
 find "$folder" -type f -name 'tmp*.png' -exec rm {} \;
 
+
 ## errori da correggere, per stranezze dei PDF, con giorni in più rispetto al mese
 mlr -I --csv head -n 29 "$folder"/data/2020-02-15_migranti_sbarcati_per_giorno.csv
 mlr -I --csv head -n 30 "$folder"/data/2020-09-15_migranti_sbarcati_per_giorno.csv
 mlr -I --csv head -n 30 "$folder"/data/2023-04-15_migranti_sbarcati_per_giorno.csv
 
+
 # check giorni mese
 mlr --csv put '$mese=strftime(strptime($Data,"%Y-%m-%d"),"%m");$anno=strftime(strptime($Data,"%Y-%m-%d"),"%Y")' then cut -f Data_Report,mese,anno then count-similar -g Data_Report,mese then uniq -a then sort -f Data_Report "$folder"/data/*_migranti_sbarcati_per_giorno.csv >"$folder"/tmp/tmp_all.csv
 
 mlr --csv join --ul --np -j anno,mese,count -f "$folder"/tmp/tmp_all.csv then unsparsify "$folder"/risorse/anagrafica_anni_mesi.csv > "$folder"/tmp/errori_giorni_mese.csv
+
+# sposta tutti i file CSV presenti in "$folder"/data/ nella cartella rawdata/csv/sbarcati-per-giorno/
+find "$folder"/data -type f -name '*sbarcati_per_giorno.csv' -exec mv {} "$folder"/../../"$progetto"/rawdata/csv/sbarcati-per-giorno/ \;
+
+exit 0
