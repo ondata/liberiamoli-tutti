a55d2306e563ec9dbe8154ee59365c90b4cab4dd
aborruso <aborruso@gmail.com>
2024-04-16 09:50:37 +0200

    update

--- sbarchi-migranti/script/bulk_sbarchi-migranti_nazionalita.sh
+++ sbarchi-migranti/script/bulk_sbarchi-migranti_nazionalita.sh
@@ -18,7 +18,7 @@ mkdir -p "$folder"/data
 # estrai la lista dei file pdf, in cui è presente il dato dei sbarchi giornalieri. Ci sono dal report del 15/10/2019
 # rimuovi il report del 15/07/2021, perché il PDF è danneggiato
 # rimuovi la prima riga perché è il dato giornaliero, tieni soltanto i quindicinali
-mlrgo --jsonl filter '$data>"2019-09-16"' then filter -x '$data=="2021-07-15"' then sort -f data "$folder"/data/cruscotto-statistico-giornaliero_lista.jsonl | head -n -1 >"$folder"/data/cruscotto-statistico-giornaliero_lista_dati_giornalieri.jsonl
+mlrgo --jsonl filter '$data>"2019-09-16"' then filter -x '$data=="2021-07-15"' then sort -f data "$folder"/data/cruscotto-statistico-giornaliero_lista.jsonl | head -n -0 >"$folder"/data/cruscotto-statistico-giornaliero_lista_dati_giornalieri.jsonl
 
 
 
