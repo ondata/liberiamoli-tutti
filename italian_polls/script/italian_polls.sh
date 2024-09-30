121df2fcdeb1fa3d7dad74222492110e020db4cd
aborruso <aborruso@gmail.com>
2024-09-30 21:59:34 +0200

    pulizia

--- italian_polls/script/italian_polls.sh
+++ italian_polls/script/italian_polls.sh
@@ -14,7 +14,7 @@ mkdir -p "$folder"/tmp
 mkdir -p "$folder"/../data
 
 # scarica il file
-curl -ksL "https://raw.githubusercontent.com/ruggsea/llm_italian_poll_scraper/main/italian_polls.jsonl" > "$folder"/tmp/risultati_raw.jsonl
+curl -ksL "https://raw.githubusercontent.com/ruggsea/llm_italian_poll_scraper/main/italian_polls.jsonl" >"$folder"/tmp/risultati_raw.jsonl
 
 # rimuovi eventuali righe duplicate
 mlrgo -I --jsonl uniq -a "$folder"/tmp/risultati_raw.jsonl
@@ -25,7 +25,6 @@ campi=$(<"$folder"/tmp/risultati_raw.jsonl head -n 1 | jq -r 'to_entries[] | .ke
 
 # rinomina i campi che non sono nomi di partito
 mlrgo --jsonl --from "$folder"/tmp/risultati_raw.jsonl label "$campi" then cat -n then cut -x -f f_Row then sort -nr n >"$folder"/tmp/risultati_raw_long.jsonl
-
 
 # trasforma il file da wide a long
 mlrgo -I --jsonl --from "$folder"/tmp/risultati_raw_long.jsonl reshape -r "^[^f][^_]" -o partito,valore then filter -x '$valore=="null"'
@@ -40,19 +39,17 @@ mlrgo -I --jsonl uniq -a then sort -nr n "$folder"/../data/risultati_raw.jsonl
 # salva elenco record con errori in valori numerici
 mlrgo --jsonl filter -x '$valore=~"^-?\d+\.?\d*$"' then filter -x 'is_null($valore)' "$folder"/../data/risultati_raw.jsonl >"$folder"/../data/errori.jsonl
 
-
 # salva elenco record senza errori in valori numerici
 mlrgo --jsonl filter '$valore=~"^-?\d+\.?\d*$"' then filter -x 'is_null($valore)' "$folder"/../data/risultati_raw.jsonl >"$folder"/../data/risultati.jsonl
 
 # rimuovi prefisso "f_" dai nomi dei campi
-mlrgo -I --jsonl rename -r "^f_",""  "$folder"/../data/risultati.jsonl
+mlrgo -I --jsonl rename -r "^f_","" "$folder"/../data/risultati.jsonl
 
 # crea csv con nomi campo snake_case e formatta le date in ISO8601
 mlrgo --ijsonl --ocsv cat "$folder"/../data/risultati.jsonl | duckdb --csv -c "select * from read_csv_auto('/dev/stdin',normalize_names = TRUE,dateformat='%d/%m/%Y')" >"$folder"/../data/risultati.csv
 
 # crea jsonl a partire dal csv
-mlrgo --icsv --ojsonl  count-similar -g n -o numero_partiti "$folder"/../data/risultati.csv >"$folder"/../data/risultati.jsonl
-
+mlrgo --icsv --ojsonl count-similar -g n -o numero_partiti "$folder"/../data/risultati.csv >"$folder"/../data/risultati.jsonl
 
 # crea jsonl con i metadati dei sondaggi
 mlrgo --jsonl cut -f n,data_inserimento,realizzatore,committente,titolo,_text,domanda,national_poll_rationale,national_poll,numero_partiti then uniq -a then sort -n nr "$folder"/../data/risultati.jsonl >"$folder"/../data/anagrafica.jsonl
