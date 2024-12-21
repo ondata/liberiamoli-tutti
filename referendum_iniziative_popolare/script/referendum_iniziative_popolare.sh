2cad8b89e6c59e72836c49b9d6fdb1be2fbc2ac7
aborruso <aborruso@gmail.com>
2024-12-21 18:33:55 +0100

    puliziA

--- referendum_iniziative_popolare/script/referendum_iniziative_popolare.sh
+++ referendum_iniziative_popolare/script/referendum_iniziative_popolare.sh
@@ -15,18 +15,18 @@ mkdir -p "${folder}"/tmp
 curl -s -o /dev/null -w "%{http_code}" https://pnri.firmereferendum.giustizia.it/referendum/api-portal/iniziativa/public | grep 200 || exit 1
 
 # download the json file
-curl -s https://pnri.firmereferendum.giustizia.it/referendum/api-portal/iniziativa/public > "${folder}"/../data/referendum_iniziative_popolare.json
+curl -s https://pnri.firmereferendum.giustizia.it/referendum/api-portal/iniziativa/public >"${folder}"/../data/referendum_iniziative_popolare.json
 
-<"${folder}"/../data/referendum_iniziative_popolare.json  jq -c '.content[]' > "${folder}"/../data/referendum_iniziative_popolare.jsonl
+<"${folder}"/../data/referendum_iniziative_popolare.json jq -c '.content[]' >"${folder}"/../data/referendum_iniziative_popolare.jsonl
 
 mlrgo -I --jsonl --from "${folder}"/../data/referendum_iniziative_popolare.jsonl cut -f quorum,sito,sostenitori,supportata,titolo,titoloLeggeCostituzionale,estremi,id,dataApertura,dataFineRaccolta,dataFineValidita,dataGazzetta,dataInizioRaccolta,dataInizioValidita,dataIns,dataUltimoAgg,descrizione then put '$url="https://pnri.firmereferendum.giustizia.it/referendum/open/dettaglio-open/".$id'
 
 # get the current datetime in the format YYYYMMDDHHMMSS ISO8601
 datetime=$(date '+%Y-%m-%dT%H:%M:%S')
 
-mlrgo --jsonl cut -f id,sostenitori then put '$datetime="'"$datetime"'"'  then sort -f id,datetime "${folder}"/../data/referendum_iniziative_popolare.jsonl>> "${folder}"/../data/referendum_iniziative_popolare_log.jsonl
+mlrgo --jsonl cut -f id,sostenitori then put '$datetime="'"$datetime"'"' then sort -f id,datetime "${folder}"/../data/referendum_iniziative_popolare.jsonl >>"${folder}"/../data/referendum_iniziative_popolare_log.jsonl
 
-mlrgo -I --jsonl uniq -a then sort -n id -t datetime  "${folder}"/../data/referendum_iniziative_popolare_log.jsonl
+mlrgo -I --jsonl uniq -a then sort -n id -t datetime "${folder}"/../data/referendum_iniziative_popolare_log.jsonl
 
 mlrgo --ijsonl --ocsv cat "${folder}"/../data/referendum_iniziative_popolare_log.jsonl >"${folder}"/../data/referendum_iniziative_popolare_log.csv
 
