b416043ceb6caefe6d56c46ca389b92e168ce4ee
aborruso <aborruso@gmail.com>
2025-03-16 22:54:11 +0100

    update

--- pnrr_cup_cig/script/pnrr_cup_cig.sh
+++ pnrr_cup_cig/script/pnrr_cup_cig.sh
@@ -11,6 +11,8 @@ cup_cig_anac="https://dati.anticorruzione.it/opendata/download/dataset/cup/files
 
 progetti_pnrr="https://www.italiadomani.gov.it/content/dam/sogei-ng/opendata/PNRR_Progetti.csv"
 
+gare_pnrr="https://www.italiadomani.gov.it/content/dam/sogei-ng/opendata/PNRR_Gare.csv"
+
 mkdir -p "${folder}"/../data
 mkdir -p "${folder}"/tmp
 
@@ -33,6 +35,13 @@ else
   wget -O "${folder}"/tmp/PNRR_Progetti.csv "${progetti_pnrr}"
 fi
 
+# se il file esiste già non lo scarica
+if [ -f "${folder}"/tmp/PNRR_Gare.csv ]; then
+  echo "File PNRR_Gare.csv already exists, skipping download."
+else
+  wget -O "${folder}"/tmp/PNRR_Gare.csv "${gare_pnrr}"
+fi
+
 # Estrai la colonna CUP di PNRR_Progetti.csv, e fai il join con la colonna CUP di cup_csv.csv, ed estrai soltanto CUP e CIG
 duckdb --csv -c "SELECT DISTINCT pnrr.CUP, cup.CIG
 FROM read_csv('${folder}/tmp/PNRR_Progetti.csv') AS pnrr
@@ -43,4 +52,18 @@ ORDER BY pnrr.CUP, cup.CIG" >"${folder}"/../data/cup_cig_anac_pnrr.csv
 # crea anche versione parquet
 duckdb -c "copy (select * from read_csv('${folder}/../data/cup_cig_anac_pnrr.csv',sample_size=-1)) TO '${folder}/../data/cup_cig_anac_pnrr.parquet' (FORMAT 'parquet', COMPRESSION 'zstd', ROW_GROUP_SIZE 100_000)"
 
-rm "${folder}"/tmp/cup_csv.zip
+# estrai tutti i CUP present nel file PNRR_Gare.csv, non presenti in cup_csv.csv
+duckdb --csv -c "SELECT DISTINCT pnrr.CUP,pnrr.CIG,pnrr.\"CIG Accordo Quadro\" CIG_Accordo_Quadro
+FROM read_csv('${folder}/tmp/PNRR_Gare.csv') AS pnrr
+LEFT JOIN read_csv('${folder}/tmp/cup_csv.csv') AS cup
+ON pnrr.CUP = cup.CUP
+WHERE cup.CUP IS NULL
+ORDER BY pnrr.CUP" >"${folder}"/../data/cup_cig_pnrr_no-anac.csv.csv
+
+mlr --csv put '$fonte="italiadomani"' "${folder}"/../data/cup_cig_pnrr_no-anac.csv.csv >"${folder}"/tmp/id.csv
+
+mlr --csv put '$fonte="anac"' "${folder}"/../data/cup_cig_anac_pnrr.csv >"${folder}"/tmp/anac.csv
+
+mlr --csv unsparsify then cut -f CUP,CIG,fonte then sort -f CUP,CIG then filter '$CIG=~".+"' then filter -x '$CUP=="N/A" || $CIG=="NULL"' "${folder}"/tmp/id.csv "${folder}"/tmp/anac.csv > "${folder}"/../data/cup_cig_anac_pnrr_merge.csv
+
+rm -f "${folder}"/tmp/*
