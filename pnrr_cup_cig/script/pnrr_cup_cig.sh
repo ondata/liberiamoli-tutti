a052151847e2a1aedcf7e6be24821ddb6e063756
aborruso <aborruso@gmail.com>
2025-03-16 20:22:06 +0100

    cup cig pnrr anac

---
+++ pnrr_cup_cig/script/pnrr_cup_cig.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cup_cig_anac="https://dati.anticorruzione.it/opendata/download/dataset/cup/filesystem/cup_csv.zip"
+
+progetti_pnrr="https://www.italiadomani.gov.it/content/dam/sogei-ng/opendata/PNRR_Progetti.csv"
+
+mkdir -p "${folder}"/../data
+mkdir -p "${folder}"/tmp
+
+# svuota la cartella tmp
+rm -f "${folder}"/tmp/*
+
+# scarica i file in tmp
+# se il file esiste già non lo scarica
+if [ -f "${folder}"/tmp/cup_csv.zip ]; then
+  echo "File cup_csv.zip already exists, skipping download."
+else
+  wget -O "${folder}"/tmp/cup_csv.zip "${cup_cig_anac}"
+  unzip -o "${folder}"/tmp/cup_csv.zip -d "${folder}"/tmp
+fi
+
+# se il file esiste già non lo scarica
+if [ -f "${folder}"/tmp/PNRR_Progetti.csv ]; then
+  echo "File PNRR_Progetti.csv already exists, skipping download."
+else
+  wget -O "${folder}"/tmp/PNRR_Progetti.csv "${progetti_pnrr}"
+fi
+
+# Estrai la colonna CUP di PNRR_Progetti.csv, e fai il join con la colonna CUP di cup_csv.csv, ed estrai soltanto CUP e CIG
+duckdb --csv -c "SELECT DISTINCT pnrr.CUP, cup.CIG
+FROM read_csv('${folder}/tmp/PNRR_Progetti.csv') AS pnrr
+JOIN read_csv('${folder}/tmp/cup_csv.csv') AS cup
+ON pnrr.CUP = cup.CUP
+ORDER BY pnrr.CUP, cup.CIG" >"${folder}"/../data/cup_cig_anac_pnrr.csv
+
+# crea anche versione parquet
+duckdb -c "copy (select * from read_csv('${folder}/../data/cup_cig_anac_pnrr.csv',sample_size=-1)) TO '${folder}/../data/cup_cig_anac_pnrr.parquet' (FORMAT 'parquet', COMPRESSION 'zstd', ROW_GROUP_SIZE 100_000)"
+
+rm "${folder}"/tmp/cup_csv.zip
