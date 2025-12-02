00518876399118f37430b700750325b6d80ed6c4
bluefernet <enrique.taietta@gmail.com>
2025-12-02 01:24:28 +0100

    Update url Cruscotto Statistico e scrape class di riferimento

--- sbarchi-migranti/script/bulk_download_pdf.sh
+++ sbarchi-migranti/script/bulk_download_pdf.sh
@@ -13,21 +13,22 @@ mkdir -p "$folder"/tmp
 mkdir -p "$folder"/tmp/check
 mkdir -p "$folder"/data
 
-URL="http://www.libertaciviliimmigrazione.dlci.interno.gov.it/it/documentazione/statistica/cruscotto-statistico-giornaliero"
+URL="https://libertaciviliimmigrazione.dlci.interno.gov.it/documentazione/dati-e-statistiche/cruscotto-statistico-giornaliero"
 
 # se URL non risponde esci
 curl -kL -s -f -o /dev/null "$URL" || exit 0
-
+echo "INIZIO"
 # estrai la lista dei file pdf presenti nella pagina
-curl -kL "$URL" | scrape -be "//div[contains(@class, 'field-item')]//a[contains(@href, '.pdf')]" | xq -c '.html.body.a[]' | tail -n +2 >"$folder"/data/cruscotto-statistico-giornaliero_lista_raw.jsonl
+curl -kL "$URL" | scrape -be "//div[contains(@class, 'file--application-pdf')]//a[contains(@href, '.pdf')]" | xq -c '.html.body.a[]' | tail -n +2 >"$folder"/data/cruscotto-statistico-giornaliero_lista_raw.jsonl
 
+echo "ERRORE 1"
 # aggiungi la data di pubblicazione del report, in formato YYYY-MM-DD. Se non è presente, usa la data di default 30-08-2000
 # rimuovi file pdf del giorno, mantieni soltanto i quindicinali
 
 mlrgo -I -S --jsonl sub -f "@title" "([0-9]{2})\.([0-9]{2})\.([0-9]{4})" "\1-\2-\3" "$folder"/data/cruscotto-statistico-giornaliero_lista_raw.jsonl
-
+echp "ERRORE 2"
 mlrgo --jsonl put '$data=strftime(strptime(regextract_or_else(${@title},"[0-9]{2}-[0-9]{2}-[0-9]{4}","30-08-2000"),"%d-%m-%Y"),"%Y-%m-%d")' "$folder"/data/cruscotto-statistico-giornaliero_lista_raw.jsonl >"$folder"/data/cruscotto-statistico-giornaliero_lista.jsonl
-
+echo "ERRORE 3"
 # estrai la lista dei file pdf, in cui è presente il dato dei sbarchi giornalieri. Ci sono dal report del 15/10/2019
 # rimuovi il report del 15/07/2021, perché il PDF è danneggiato
 # rimuovi la prima riga perché è quella del giorno. Teniamo soltanto i dati quindicinali
